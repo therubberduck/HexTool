0a1ff8e41576bf2b609e01cdb53638fabea1f5dc
TheRubberduck <therubberduckmail@gmail.com>
2018-12-27 04:18:51 +0100

    Added some MVVM binding debugging tools
    From https://spin.atomicobject.com/2013/12/11/wpf-data-binding-debug/

--- HexTool/App.xaml.cs
+++ HexTool/App.xaml.cs
@@ -1,7 +1,8 @@
-using HexTool.Database;
+﻿using HexTool.Database;
 using HexTool.ResourceHandling;
 using HexTool.VVM;
 using System;
+using System.Diagnostics;
 using System.Windows;
 
 namespace HexTool
@@ -19,6 +20,15 @@ namespace HexTool
             InitializeComponent();
         }
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            PresentationTraceSources.Refresh();
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new ConsoleTraceListener());
+            PresentationTraceSources.DataBindingSource.Listeners.Add(new DebugTraceListener());
+            PresentationTraceSources.DataBindingSource.Switch.Level = SourceLevels.Warning | SourceLevels.Error;
+            base.OnStartup(e);
+        }
+
         [STAThread]
         static void Main()
         {
@@ -34,6 +44,19 @@ namespace HexTool
             app.MainWindow = rootVm.Window;
             app.MainWindow.Show();
             app.Run();
+        }
+    }
+
+    //From https://spin.atomicobject.com/2013/12/11/wpf-data-binding-debug/
+    public class DebugTraceListener : TraceListener
+    {
+        public override void Write(string message)
+        {
+        }
+
+        public override void WriteLine(string message)
+        {
+            Debugger.Break();
         }
     }
 }
